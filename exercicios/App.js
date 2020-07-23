42c104125e5c15c4d4857c782c73104f464efc66
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-07-23 13:42:35 -0400

    Create Button

--- exercicios/App.js
+++ exercicios/App.js
@@ -1,19 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
-import Comp, { Comp1, Comp2 } from "./src/components/Multi";
-import Aleatorio from "./src/components/Aleatorio";
+import Botao from "./src/components/Botao";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Aleatorio max={30} min={3} />
-      <Aleatorio max={30} min={3} />
-      <Aleatorio max={30} min={3} />
-      <Aleatorio max={30} min={3} />
-      <Aleatorio max={30} min={3} />
-      <Aleatorio max={30} min={3} />
-      <Aleatorio max={30} min={3} />
+      <Botao />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
