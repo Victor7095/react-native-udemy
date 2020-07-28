c93f1d7bbd521e9114ff150fee7d6c329f07045f
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-07-28 18:46:37 -0400

    Lottery Draw Exercise #1

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
 
 export default class Mega extends Component {
   state = {
     qtdeNumeros: this.props.qtdeNumeros + "",
+    numeros: [],
+  };
+
+  gerarNumeros = () => {
+    const numeros = [];
+    while (numeros.length < this.state.qtdeNumeros) {
+      const novoNumero = Math.floor(Math.random() * 100);
+      !numeros.includes(novoNumero) && numeros.push(novoNumero);
+    }
+    this.setState({ numeros: numeros.sort((a, b) => a - b ) });
   };
 
   render() {
@@ -14,8 +24,12 @@ export default class Mega extends Component {
           keyboardType="numeric"
           style={{ borderBottomWidth: 1 }}
           value={this.state.qtdeNumeros}
-          onChangeText={qtde => this.setState({qtdeNumeros: qtde})}
+          onChangeText={(qtde) => this.setState({ qtdeNumeros: qtde })}
         />
+        <Button onPress={this.gerarNumeros} title="Click"></Button>
+        {this.state.numeros.map((num, i) => (
+          <Text key={i}>{num}</Text>
+        ))}
       </>
     );
   }
